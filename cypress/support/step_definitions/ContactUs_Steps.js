6a795ff436338e4a79d4ce2f8802ccd14967b924
tielefernandes <tielefernandes@gmail.com>
2024-02-01 08:44:45 -0300

    :test_tube: test: new test using cucumber regular expressions

--- cypress/support/step_definitions/ContactUs_Steps.js
+++ cypress/support/step_definitions/ContactUs_Steps.js
@@ -29,3 +29,19 @@ Then(`I should be presented with a successful contact us submission message`, ()
 Then(`I should be presented with a unsuccessful contact us submission message`, () => {
     cy.get('body').contains('Error: Invalid email address');
 })
+
+When(`I type specific first name {string}`, (firtsName) => {
+	cy.get('[name="first_name"]').type(firtsName);
+})
+
+When(`I type specific last name {string}`, (lastName) => {
+	cy.get('[name="last_name"]').type(lastName);
+})
+
+When(`I type a specific email address {string}`, (emailAddress) => {
+	cy.get('[name="email"]').type(emailAddress);
+})
+
+When(`I type a specific word {string} and number {int} within the comment input field`, (word, number) => {
+	cy.get('textarea[name="message"]').type(word + " " + number);
+})
