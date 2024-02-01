0aeb28370029803f322f128abfe4ca9533665c44
tielefernandes <tielefernandes@gmail.com>
2024-02-01 11:04:06 -0300

    :test_tube: test: add new test using scneario outline example

--- cypress/support/step_definitions/ContactUs_Steps.js
+++ cypress/support/step_definitions/ContactUs_Steps.js
@@ -3,31 +3,31 @@
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 When(`I type a first name`, () => {
-    cy.get('[name="first_name"]').type("Rachel");
+	cy.get('[name="first_name"]').type("Rachel");
 })
 
 When(`I type a last name`, () => {
-    cy.get('[name="last_name"]').type("Green");
+	cy.get('[name="last_name"]').type("Green");
 })
 
 When(`I enter an email address`, () => {
-    cy.get('[name="email"]').type("rachel.green@email.com");
+	cy.get('[name="email"]').type("rachel.green@email.com");
 })
 
 When(`I type a comment`, () => {
-    cy.get('textarea[name="message"]').type("Hello World!");
+	cy.get('textarea[name="message"]').type("Hello World!");
 })
 
 When(`I click on the submit button`, () => {
-    cy.get('[type="submit"]').click();
+	cy.get('[type="submit"]').click();
 })
 
 Then(`I should be presented with a successful contact us submission message`, () => {
-    cy.get('h1').should('have.text', 'Thank You for your Message!');
+	cy.get('h1').should('have.text', 'Thank You for your Message!');
 })
 
 Then(`I should be presented with a unsuccessful contact us submission message`, () => {
-    cy.get('body').contains('Error: Invalid email address');
+	cy.get('body').contains('Error: Invalid email address');
 })
 
 When(`I type specific first name {string}`, (firtsName) => {
@@ -44,4 +44,18 @@ When(`I type a specific email address {string}`, (emailAddress) => {
 
 When(`I type a specific word {string} and number {int} within the comment input field`, (word, number) => {
 	cy.get('textarea[name="message"]').type(word + " " + number);
+})
+
+When(`I type a first name {string} and a last name {string}`, (firtsName, lastName) => {
+	cy.get('[name="first_name"]').type(firtsName);
+	cy.get('[name="last_name"]').type(lastName);
+})
+
+When(`I type a {string} and a comment {string}`, (emailAddress, comment) => {
+	cy.get('[name="email"]').type(emailAddress);
+	cy.get('textarea[name="message"]').type(comment);
+})
+
+Then(`I should be presented with header text {string}`, (message) => {
+	cy.xpath("//h1 | //body").contains(message);
 })
